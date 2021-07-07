80bafb706d338d15e624ad1f7b7af0aeade0c305
dheera <dheera@dheera.net>
2021-07-07 04:05:55 +0000

    setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+package_name = 'rosboard'
+
+setup(
+    name=package_name,
+    version='1.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    #zip_safe=True,
+    maintainer='dheera',
+    maintainer_email='dheera.r.e.m.o.v.e.t.h.i.s@dheera.net'
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "rosboard_node = rosboard.rosboard:main",
+        ],
+    },
+)
