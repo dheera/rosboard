817549937e7185e7a64dbf553d26f5ae0b518f24
dheera <dheera@dheera.net>
2021-07-14 08:15:35 +0000

    add geometry_msgs/Polygon

---
+++ rosboard/html/js/viewers/PolygonViewer.js
@@ -0,0 +1,38 @@
+"use strict";
+
+importJsOnce("js/viewers/Space2DViewer.js");
+
+class PolygonViewer extends Space2DViewer {
+  onData(msg) {
+      this.card.title.text(msg._topic_name);
+
+      let polygon = msg;
+      if(msg._topic_type.endsWith("Stamped")) {
+        polygon = msg.polygon;
+      }
+
+      let points = new Float32Array((polygon.points.length + 1) * 2);
+      
+      for(let i = 0; i < polygon.points.length; i++) {
+        console.log(polygon.points[i].x, polygon.points[i].y);
+        points[2*i] = polygon.points[i].x;
+        points[2*i+1] = polygon.points[i].y;
+      }
+      points[2*polygon.points.length] = polygon.points[0].x;
+      points[2*polygon.points.length+1] = polygon.points[0].y;
+      this.draw([
+        {type: "path", data: [0, 0, 0, 1], color: "#00f060", lineWidth: 2},
+        {type: "path", data: [0, 0, 1, 0], color: "#f06060", lineWidth: 2},
+        {type: "path", data: points, color: "#e0e0e0"},
+      ]);
+  }
+}
+
+PolygonViewer.supportedTypes = [
+    "geometry_msgs/msg/Polygon",
+    "geometry_msgs/msg/PolygonStamped",
+];
+
+PolygonViewer.maxUpdateRate = 10.0;
+
+Viewer.registerViewer(PolygonViewer);
