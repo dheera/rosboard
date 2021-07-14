a7ae4b5a4de31e0130cd964c3a5c1c74f9bfe7d1
dheera <dheera@dheera.net>
2021-07-13 21:00:43 -0700

    preliminary LaserScan support

--- rosboard/html/js/viewers/LaserScanViewer.js
+++ rosboard/html/js/viewers/LaserScanViewer.js
@@ -1,20 +1,24 @@
 "use strict";
 
-class LaserScanViewer extends Viewer {
-  /**
-    * Gets called when Viewer is first initialized.
-    * @override
-  **/
-  onCreate() {
-    this.viewer = $('<div></div>')
-      .css({'font-size': '11pt'
-    , "filter": "invert(100%) saturate(50%)"})
-      .appendTo(this.card.content);
-  }
+importJsOnce("js/viewers/Space2DViewer.js");
 
+class LaserScanViewer extends Space2DViewer {
   onData(msg) {
       this.card.title.text(msg._topic_name);
-      
+
+      let angles = [];
+      let angle_incr = (msg.angle_max - msg.angle_min) / msg.ranges.length;
+      let points = [];
+      for(let i = 0; i < msg.ranges.length; i++) {
+        let angle = msg.angle_min + i * angle_incr;
+        if(-10000.0 < msg.ranges[i] && msg.ranges[i] < 10000.0) {
+          points.push([msg.ranges[i] * Math.cos(angle), msg.ranges[i] * Math.sin(angle)]);
+        }
+      }
+      this.draw([
+        ["points", points],
+      ]);
+
   }
 }
 
---
+++ rosboard/html/js/viewers/Space2DViewer.js
@@ -0,0 +1,87 @@
+"use strict";
+
+class Space2DViewer extends Viewer {
+  /**
+    * Gets called when Viewer is first initialized.
+    * @override
+  **/
+  onCreate() {
+
+    this.wrapper = $('<div></div>')
+      .css({
+        "position": "relative",
+        "width": "100%",
+      })
+      .appendTo(this.card.content);
+
+    this.wrapper2 = $('<div></div>')
+      .css({
+        "width": "100%",
+        "padding-bottom": "100%",
+        "background": "#303035",
+        "position": "relative",
+        "overflow": "hidden",
+      })
+      .appendTo(this.wrapper);
+
+    this.size = 500;
+    this.xmin = -10;
+    this.xmax = 10;
+    this.ymin = -10;
+    this.ymax = 10;
+
+    this.canvas = $('<canvas width="' + this.size + '" height="' + this.size + '"></canvas>')
+      .css({"width": "100%", "height": "100%", "position": "absolute", "top": 0, "left": 0, "right": 0, "bottom": 0})
+      .appendTo(this.wrapper2);
+
+    this.ctx = this.canvas[0].getContext("2d");
+  }
+
+  draw(commands) {
+
+    let x2px = (x) => this.size * ((x - this.xmin) / (this.xmax - this.xmin));
+    let y2py = (y) => this.size * (1 - (y - this.ymin) / (this.ymax - this.ymin));
+
+    this.ctx.clearRect(0, 0, this.size, this.size);
+
+    this.ctx.fillStyle = "#303035";
+    this.ctx.fillRect(0, 0, this.size, this.size);
+
+    // draw grid
+    this.ctx.strokeStyle = "#505050";
+    this.ctx.beginPath();
+
+    for(let x=this.xmin;x<=this.xmax+0.001;x+=1.0) {
+        this.ctx.moveTo(x2px(x),y2py(this.ymin));
+        this.ctx.lineTo(x2px(x),y2py(this.ymax));
+    }
+
+    for(let y=this.ymin;y<=this.ymax+0.001;y+=1.0) {
+        this.ctx.moveTo(x2px(this.xmin),y2py(y));
+        this.ctx.lineTo(x2px(this.xmax),y2py(y));
+    }
+
+    this.ctx.stroke();
+
+
+    this.ctx.fillStyle = "#d0d0d0";
+
+    for(let i in commands) {
+      let command = commands[i];
+      if(command[0] === "line") {
+        // draw a line
+      } else if(command[0] === "points") {
+        for(let i in command[1]) {
+          this.ctx.fillRect(x2px(command[1][i][0])-2, y2py(command[1][i][1])-2,5,5);
+        }
+      }
+    }
+  }
+}
+
+Space2DViewer.supportedTypes = [
+];
+
+Space2DViewer.maxUpdateRate = 10.0;
+
+Viewer.registerViewer(Space2DViewer);
