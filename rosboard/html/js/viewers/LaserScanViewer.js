e05b29b78294849c72d8b90396384085deaa1090
dheera <dheera@dheera.net>
2021-07-15 04:40:28 +0000

    comments

--- rosboard/html/js/viewers/LaserScanViewer.js
+++ rosboard/html/js/viewers/LaserScanViewer.js
@@ -6,9 +6,15 @@ class LaserScanViewer extends Space2DViewer {
   onData(msg) {
       this.card.title.text(msg._topic_name);
 
-      let angles = [];
+      // angle increment between points
       let angle_incr = (msg.angle_max - msg.angle_min) / msg.ranges.length;
+
+      // pre-allocate an array for xy points that we will feed into the plotter
+      // format is [x0, y0, x1, y1, x2, y2, ...]
+      // (i suppose JS doesn't have pre-allocated 2D arrays without an insane number of lambda calls (?))
       let points = new Float32Array(msg.ranges.length * 2);
+
+      // convert all the angles/ranges to x,y and fill in the points array
       for(let i = 0; i < msg.ranges.length; i++) {
         let angle = msg.angle_min + i * angle_incr;
         if(-10000.0 < msg.ranges[i] && msg.ranges[i] < 10000.0) {
@@ -19,10 +25,12 @@ class LaserScanViewer extends Space2DViewer {
           points[2*i+1] = NaN;
         }
       }
+
+      // send it to the plotter to display
       this.draw([
-        {type: "path", data: [0, 0, 0, 1], color: "#00f060", lineWidth: 2},
-        {type: "path", data: [0, 0, 1, 0], color: "#f06060", lineWidth: 2},
-        {type: "points", data: points, color: "#e0e0e0"},
+        {type: "path", data: [0, 0, 0, 1], color: "#00f060", lineWidth: 2}, // unit x axis visualization in red
+        {type: "path", data: [0, 0, 1, 0], color: "#f06060", lineWidth: 2}, // unit y axis visualization in green
+        {type: "points", data: points, color: "#e0e0e0"}, // the laserscan points to be plotted
       ]);
   }
 }
