ed5e7e4892259221b6275dba6458441218b18b58
dheera <dheera@dheera.net>
2021-07-19 18:02:03 +0000

    increment version and document in changelog

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = 'rosboard'
 
 setup(
     name=package_name,
-    version='1.1.3',
+    version='1.2.0',
     packages=find_packages(), #[package_name],
     data_files=[
         ('share/' + package_name, ['package.xml']),
