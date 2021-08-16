1b2e6779de86c3700cfc2182bd0994a07f3880d2
dheera <dheera@dheera.net>
2021-08-16 00:16:15 +0000

    add system stats subscriber

--- rosboard/subscribers/system_stats_subscriber.py
+++ rosboard/subscribers/system_stats_subscriber.py
@@ -1,50 +1,69 @@
 #!/usr/bin/env python3
 
-import psutil
-import select
-import subprocess
+try:
+    import psutil
+except (ImportError, ModuleNotFoundError) as e:
+    psutil = None
+
 import time
 import threading
 import traceback
 
+def mean(list):
+    return sum(list)/len(list)
+
 class SystemStatsSubscriber(object):
     def __init__(self, callback):
         self.callback = callback
-        self.process = None
+        self.stop = False
         threading.Thread(target = self.start, daemon = True).start()
 
     def __del__(self):
-        if self.process:
-            self.process.terminate()
-            self.process = None
+        self.stop = True
+        pass
 
     def unregister(self):
-        if self.process:
-            self.process.terminate()
-            self.process = None
+        self.stop = True
+        pass
 
     def start(self):
-        try:
-            self.process = subprocess.Popen(['dmesg', '--follow'], stdout = subprocess.PIPE)
-            p = select.poll()
-            p.register(self.process.stdout, select.POLLIN)
-    
-            while True:
-                time.sleep(0.1)
+        if psutil is None:
+            self.callback({"_error": "Please install psutil (sudo pip3 install --upgrade psutil) to use this feature."})
+            return
 
-                if self.process is None:
-                    break
+        while not self.stop:
+            try:
+                p = psutil.Process()
+
+                with p.oneshot():
+                    sensors_temperatures = psutil.sensors_temperatures()
+                    cpu_percent = psutil.cpu_percent(percpu = True)
+                    net_io_counters = psutil.net_io_counters()
+                    virtual_memory = psutil.virtual_memory()
+                    swap_memory = psutil.swap_memory()
+                    disk_usage = psutil.disk_usage('/')
+
+                status = {}
+
+                status["cpu_percent"] = cpu_percent
+
+                if "coretemp" in sensors_temperatures:
+                    status["temp_coretemp"] = mean(list(map(
+                        lambda x:x.current,
+                        sensors_temperatures["coretemp"]
+                    )))
                 
-                lines = []
-                while p.poll(1):
-                    lines.append(self.process.stdout.readline().decode('utf-8').strip())
+                status["net_bytes_sent"] = net_io_counters.bytes_sent
+                status["net_bytes_recv"] = net_io_counters.bytes_recv
+                status["disk_usage_percent"] = disk_usage.percent
+                status["virtual_memory_percent"] = virtual_memory.percent
+                status["swap_memory_percent"] = swap_memory.percent
 
-                text = "\n".join(lines)
-                if len(text) > 0:
-                    self.callback("\n".join(lines))
-        except:
-            traceback.print_exc()
+            except Exception as e:
+                traceback.print_exc()
 
+            self.callback(status)
+            time.sleep(3)
 
 if __name__ == "__main__":
     # Run test
